73be351998789dbf6706953ce00e6ed0d6d7c5d7
Rahul Kumar <rahul.k@exzeo.com>
2016-08-17 16:37:45 +0530

    added routes

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 import Cart from 'Cart';
 
+var data = ['test1','test2'];
 
-ReactDOM.render(
-    <Cart productNames={['test','test2']}/>,
-    document.getElementById("app")
-);
+
+	ReactDOM.render(
+			
+			<Router history={hashHistory}>
+        		<Route path="/" component={() => <Cart productNames={data}/>} >
+           		 <IndexRoute component={() => <Cart productNames={data}/>} />
+        		</Route>
+        		<Route path="/checkout" component={() => <Cart productNames={data}/>} >
+           		 <IndexRoute component={() => <Cart productNames={data}/>} />
+        		</Route>
+   			</Router>,  document.getElementById("app")
+	);
