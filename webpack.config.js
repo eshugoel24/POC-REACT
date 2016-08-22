ce9df3b50544d2ea8d5a38af0421d33ab8407b4e
Chandan Kumar <chandank@exzeo.com>
2016-08-22 15:23:36 +0530

    adding webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,9 @@ module.exports = {
         alias: {
             MainLayout: 'app/components/MainLayout.jsx',
 			applicationStyles: 'app/styles/app.scss',
-			Dashboard: 'app/components/dashboard/Dashboard.jsx'
+			Dashboard: 'app/components/dashboard/Dashboard.jsx',
+            Products:'app/components/admin/product.jsx',
+            ProductCategory: 'app/components/admin/category.jsx'
         },
         extensions: ['','.js','.jsx']
     },
