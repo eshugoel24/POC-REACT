23187231e86489abc4bd5e9f09075a00c2e71474
gagand <gagand@exzeoindia.com>
2016-09-16 12:35:30 +0530

    Basic Validation Implemented

--- app/components/account/login.jsx
+++ app/components/account/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import {browserHistory} from 'react-router';
+import ReactDOM from 'react-dom'
 
 import 'dist/stylesheets/main.css'
 
@@ -14,8 +15,24 @@ class Login extends React.Component {
     _onSubmit(e) {
         var self = this;
         e.preventDefault();
+        //Todo: Implement react-validation
+        if(self.refs.password.value === '' && self.refs.username.value === ''){
+              ReactDOM.findDOMNode(this.refs.errusername).setAttribute('style','display:block');
+              ReactDOM.findDOMNode(this.refs.errpassword).setAttribute('style','display:block');
+        }
+        else if(self.refs.password.value === ''){
+              ReactDOM.findDOMNode(this.refs.errpassword).setAttribute('style','display:block');
+              ReactDOM.findDOMNode(this.refs.errusername).setAttribute('style','display:none');
+        }
+        else if(self.refs.username.value === ''){
+            ReactDOM.findDOMNode(this.refs.errusername).setAttribute('style','display:block');
+            ReactDOM.findDOMNode(this.refs.errpassword).setAttribute('style','display:none');  
+        }
+        else{
+            ReactDOM.findDOMNode(this.refs.errusername).setAttribute('style','display:none');
+            ReactDOM.findDOMNode(this.refs.errpassword).setAttribute('style','display:none');
 
-        axios.post('api/auth', { username: self.refs.username.value, password: self.refs.password.value })
+            axios.post('api/auth', { username: self.refs.username.value, password: self.refs.password.value })
             .then(function (response) {
                 if (response.status === 200 && response.data.success) {
                      browserHistory.push('/dashboard');
@@ -26,7 +43,8 @@ class Login extends React.Component {
             }).catch(function (err) {
                 self.setState({ message: 'something went wrong, try after some time' });
             });
-
+        }
+        
     }
 
     render() {
@@ -36,7 +54,9 @@ class Login extends React.Component {
             <p>Fill out the form below to login to my super awesome imaginary control panel.</p>
             <form onSubmit={this._onSubmit}>
                 <input className="type-text" type='text' ref='username' placeholder='username'></input><br/>
+                <span style={{display: 'none' }} ref="errusername">UserName is required</span>
                 <input className="type-text" type='password' ref='password' placeholder='password'></input><br/>
+                <span style={{display: 'none' }} ref="errpassword">Password is required</span>
                 <button className="btn btn-primary" type='submit'>Login</button>
                 <p style={{ color: 'red' }}>{this.state.message}</p>
             </form>
